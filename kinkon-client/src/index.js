e0c16ff7d14d79938f512529073a93a983eb62bd
Anthony Ashurst <anthoasho@gmail.com>
2018-03-23 09:15:39 +0000

    remove redundant files

--- kinkon-client/src/index.js
+++ kinkon-client/src/index.js
@@ -3,9 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import setAuthToken from "./utils/setAuthToken";
-
-setAuthToken(localStorage.jwtToken);
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
